7f701e2673e9e30940b23f70454f0ecfdd3a8b47
realtin <juliakbln@gmail.com>
2015-12-16 22:00:57 +0100

    add functions SAV, SWP
    also read multiple commands with read-commands

--- src/skinner/core.clj
+++ src/skinner/core.clj
@@ -4,18 +4,41 @@
 (def memory (atom 0))
 ; BAK
 (def temp-storage (atom 0))
+
 ; make sure that my atoms aren't nil
 (defn myswap!
   [myatom fn]
-  (if (= (@myatom) nil)
+  (if (= @myatom nil)
     (reset! myatom 0)
     (swap! myatom fn)))
+
 ; Language Parser
 (defmulti parse (fn [my-var]
   (first my-var)))
+
 ; adds a value to the primary memory
 (defmethod parse :ADD [x]
   (myswap! memory (fn [memory] (+ memory (last x)))))
+
 ; subtracts a value from the primary memory
 (defmethod parse :SUB [x]
   (myswap! memory (fn [memory] (- memory (last x)))))
+
+; dublicates the value in memory (ACC) to the temporary storage (BAK)
+(defmethod parse :SAV [x]
+  (reset! temp-storage @memory))
+
+; swap ACC and BAK with eachother
+(defmethod parse :SWP [x]
+  (let [prev-memory @memory
+        prev-temp @temp-storage]
+    (reset! memory prev-temp)
+    (reset! temp-storage prev-memory)))
+
+
+(defn read-commands
+  [xs]
+  (if (not= (peek xs) nil)
+    (do (parse (peek xs))
+      (read-commands (pop xs)))))
+
